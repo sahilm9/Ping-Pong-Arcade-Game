4d052d97ede6ce7bb20258bdde903868f84267d5
sahilm9 <sahil.mokkapati@gmail.com>
2018-06-10 14:56:59 -0700

    Required variables initialized

--- app/dist/scripts/app.js
+++ app/dist/scripts/app.js
@@ -70,5 +70,27 @@
 "use strict";
 
 
+var canvas = document.getElementById("myCanvas"),
+    context = canvas.getContext("2d"),
+    width = canvas.width,
+    height = canvas.height,
+    x = canvas.width / 2,
+    y = canvas.height / 2,
+    dx,
+    dy,
+    ballRadius = 10,
+    paddleWidth = 10,
+    paddleHeight = 100,
+    leftPaddleX = 0,
+    leftPaddleY = canvas.height / 2 - 50,
+    computerPaddleY = canvas.height / 2 - 50,
+    computerPaddleX = canvas.width - paddleWidth,
+    upPressed = false,
+    downPressed = false,
+    winScore = 5,
+    playerScore = 0,
+    computerScore = 0,
+    gameOver = false;
+
 /***/ })
 /******/ ]);
